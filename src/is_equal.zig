4e7032f7e3c8cc666ec05bcb2a731ab333469662
Miika Henttonen <miika.henttonen@gmail.com>
2024-08-20 12:53:24 +0300

    Add tests for isEqual

--- src/is_equal.zig
+++ src/is_equal.zig
@@ -1,5 +1,14 @@
 const std = @import("std");
+const expect = std.testing.expect;
 
 pub fn isEqual(s: []const u8, s2: []const u8) bool {
     return std.mem.eql(u8, s, s2);
+}
+
+test "returns true when the strings are equal" {
+    try expect(isEqual("foo", "foo"));
+}
+
+test "returns false when the strings are equal" {
+    try expect(!isEqual("foo", "bar"));
 }
